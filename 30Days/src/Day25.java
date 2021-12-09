f97895d95efe90fcda1e4b9e362c6722aa3aa1ed
Austin Swack <58092884+aswack@users.noreply.github.com>
2021-12-09 10:48:35 -0600

    optimized day 25 to handle very large primes

--- 30Days/src/Day25.java
+++ 30Days/src/Day25.java
@@ -21,7 +21,7 @@ public class Day25 {
       if (num < 2 || (num % 2 == 0 && num != 2)) {
         prime = false;
       } else {
-        for (int i = 3; i <= Math.sqrt(num); i++) {
+        for (int i = 3; i <= Math.sqrt(num); i+=2) {
           if (num % i == 0) {
             prime = false;
             break;
