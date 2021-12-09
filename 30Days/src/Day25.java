94a02e6a3cf5cd84ed8f8c00ed59a8bef56f24b5
Austin Swack <58092884+aswack@users.noreply.github.com>
2021-12-09 10:33:45 -0600

    completed day 25

---
+++ 30Days/src/Day25.java
@@ -0,0 +1,35 @@
+//  HackerRank 30 Days of Code
+//  Day 25: Running Time and Complexity
+//  https://www.hackerrank.com/challenges/30-running-time-and-complexity/problem
+//  Created by Austin Swack
+//  Created on 12/9/2021
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day25 {
+
+  public static void main(String[] args) throws FileNotFoundException {
+    File file = new File("30Days/data/day25input.txt");
+    Scanner scan = new Scanner(file);
+    int testCases = scan.nextInt();
+
+    while (scan.hasNextInt()) {
+      int num = scan.nextInt();
+      boolean prime = true;
+
+      if (num < 2 || (num % 2 == 0 && num != 2)) {
+        prime = false;
+      } else {
+        for (int i = 3; i <= Math.sqrt(num); i++) {
+          if (num % i == 0) {
+            prime = false;
+            break;
+          }
+        }
+      }
+      if (prime) System.out.println("Prime");
+      else System.out.println("Not prime");
+    }
+  }
+}
